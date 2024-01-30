79173bb53f06b63d07ca618973d624aa53d05ceb
interstellarninja <ninjainterstellar@gmail.com>
2024-01-30 20:09:25 +0545

    adding prompt generator

---
+++ tool_eval/prompter.py
@@ -0,0 +1,70 @@
+from pydantic import BaseModel
+from typing import List, Dict
+from schema import Function
+from utils import (
+    get_fewshot_examples
+)
+import yaml
+import json
+import os
+
+class PromptSchema(BaseModel):
+    Role: str
+    Objective: str
+    Tools: str
+    Examples: str
+    Instructions: str 
+
+class PromptManager:
+    def __init__(self):
+        self.script_dir = os.path.dirname(os.path.abspath(__file__))
+        
+    def format_yaml_prompt(self, prompt_schema: PromptSchema, variables: Dict) -> str:
+        formatted_prompt = ""
+        for field, value in prompt_schema.dict().items():
+            if field == "Examples" and variables.get("examples") is None:
+                continue
+            formatted_value = value.format(**variables)
+            formatted_prompt += f"{formatted_value}"
+        return formatted_prompt
+
+    def read_yaml_file(self, file_path: str) -> PromptSchema:
+        with open(file_path, 'r') as file:
+            yaml_content = yaml.safe_load(file)
+        
+        prompt_schema = PromptSchema(
+            Role=yaml_content.get('Role', ''),
+            Objective=yaml_content.get('Objective', ''),
+            Tools=yaml_content.get('Tools', ''),
+            Examples=yaml_content.get('Examples', ''),
+            Instructions=yaml_content.get('Instructions', ''),
+        )
+        return prompt_schema
+    
+    def generate_prompt(self, sample, num_fewshot=None):
+        prompt_path = os.path.join(self.script_dir, 'prompt_assets', 'sys_prompt.yml')
+        prompt_schema = self.read_yaml_file(prompt_path)
+
+        if num_fewshot is not None:
+            examples = get_fewshot_examples(num_fewshot)
+        else:
+            examples = None
+
+        schema_json = json.loads(Function.schema_json())
+        schema = schema_json.get("properties", {})
+
+        variables = {
+            "tools": sample['tools'],
+            "examples": examples,
+            "schema": schema
+        }
+        sys_prompt = self.format_yaml_prompt(prompt_schema, variables)
+        sample['system'] = sys_prompt
+
+        prompt = [
+                {'content': sys_prompt, 'role': 'system'}
+            ]
+        prompt.extend(sample['prompt'])
+        return prompt
+        
+        
